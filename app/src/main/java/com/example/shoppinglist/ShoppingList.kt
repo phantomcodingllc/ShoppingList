7b70b2b0089e351217fb961e5284bf5ab89f17a0
Darrell <67837432+phantomcodingllc@users.noreply.github.com>
2025-03-20 12:00:27 -0400

    Finished list function, editing and saving functions as well. Fully working, just needs UI upgrade.

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,6 +1,7 @@
 package com.example.shoppinglist
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,9 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -63,12 +66,48 @@ fun ShoppingListApp(){
                 .fillMaxSize()
                 .padding(16.dp)
         ){
-            items(sItems){
-            ShoppingListItem(it, {}, {})
+            items(sItems){ // list
+                item ->
+                if(item.isEditing){ // checks to see if item is being edited
+                    // shopping item editor composable
+                    ShoppingItemEditor(item = item, onEditComplete = { // what happens when we are done editing
+                        editedName, editedQuantity -> // new variables for edited name and edited quantity
+
+                        // List = mapping that list and copy it
+                        sItems = sItems.map{ it.copy(isEditing = false) } // set item we are currently editing turns editing flag off
+
+
+                        // new variable for the edited item
+                        val editedItem = sItems.find{ it.id == item.id }
+                        // item.id (the item we are currently editing)
+                        // should = the it.id (item we are comparing it to) and when we find the item that has the same id
+                        // store the item in editedItem
+
+
+                        // editedItem?
+                        editedItem?.let {// let function allows us to get the shopping item (editedItem)
+                            // take the editedItem and change name
+                            it.name = editedName
+                            // take the editedItem and change quantity
+                            it.quantity = editedQuantity
+                        }
+                    } )
+                }else{
+
+                    ShoppingListItem(item = item , onEditClick = {
+                        // finding which item we are editing and changing its "isEditing" boolean to true
+                        sItems = sItems.map{ it.copy(isEditing = it.id == item.id)}
+                        // compare item id to shopping list element that is passed to us, if they
+                        // are true, then editing is true
+                    }, onDeleteClick = {
+                        // deletes current item we are looking at from list
+                        sItems = sItems - item
+                    })
+                }
                 
             }
-            }
         }
+    }
 
 
     if(showDialog){
@@ -132,6 +171,60 @@ fun ShoppingListApp(){
 }
 
 @Composable
+// when using this function, pass the (item:) you want to edit, and pass the string and int (onEditComplete: (String, Int) that you want to edit.
+fun ShoppingItemEditor(item: ShoppingItem, onEditComplete: (String, Int) -> Unit){
+
+
+    var editedName by remember { mutableStateOf(item.name) }
+    // App takes strings, not int. Change quantity from int to string or app will break
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+
+
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .background(Color.White)
+        .padding(8.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly)
+    {
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(8.dp)
+            )
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = {editedQuantity = it},
+                singleLine = true,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(8.dp)
+            )
+        }
+
+        Button(
+            onClick = {
+            isEditing = false
+            // elvis operator, if editedQuantity is null, set to default value of 1
+            onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+
+            }
+        ){
+            Text("Save")
+        }
+
+    }
+
+}
+
+
+@Composable
 fun ShoppingListItem(
     item: ShoppingItem,
 
@@ -146,7 +239,8 @@ fun ShoppingListItem(
             .border(
                 border = BorderStroke(2.dp, Color.Blue),
                 shape = RoundedCornerShape(20)
-            )
+            ),
+            horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Text(text = item.name, modifier = Modifier.padding(8.dp))
         Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
@@ -165,3 +259,4 @@ fun ShoppingListItem(
 
     }
 }
+
